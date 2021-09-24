03c5d99192f8d5ccecb0dedab9079371d3ff4cbb
Alex Reitz <alex.j.reitz@gmail.com>
2021-09-24 19:38:03 -0400

    footer

--- device-loaner/src/App.js
+++ device-loaner/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
-
+import Footer from "./Components/Footer/Footer";
 import LandingPage from "./Components/LandingPage/LandingPage";
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
+      <Footer />
     </div>
   );
 }
