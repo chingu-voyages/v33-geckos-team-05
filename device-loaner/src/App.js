d930782eb07b01e01db1df5a5e6e05bac91677fb
Ashley Christie <achristie0319@gmail.com>
2021-10-03 12:37:28 -0400

    initial commit

--- device-loaner/src/App.js
+++ device-loaner/src/App.js
@@ -1,12 +1,41 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
+import { HashRouter as Router, Route, NavLink } from "react-router-dom";
+import SignUpForm from "./pages/SignUpForm";
+import SignInForm from "./pages/SignInForm";
+
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Device Loaner</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router basename="react-auth-ui/">
+        <div className="App">
+          <div className="App__Form">
+            <div className="FormTitle">
+              <NavLink
+                to="/sign-in"
+                activeClassName="FormTitle__Link--Active"
+                className="FormTitle__Link"
+              >
+                Sign In
+              </NavLink>
+              or
+              <NavLink
+                exact
+                to="/"
+                activeClassName="FormTitle__Link--Active"
+                className="FormTitle__Link"
+              >
+                Register
+              </NavLink>
+            </div>
+            <Route exact path="/" component={SignUpForm}></Route>
+            <Route path="/sign-in" component={SignInForm}></Route>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
